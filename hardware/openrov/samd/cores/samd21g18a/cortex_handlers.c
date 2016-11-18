637fe3f6881765abb4908d51bd6f656b269c5327
Charles Cross <charles@openrov.com>
2016-11-17 20:13:26 -0800

    Added tc6 and 7 handlers back in (not sure if this is right, but arduino does it)

--- hardware/openrov/samd/cores/samd21g18a/cortex_handlers.c
+++ hardware/openrov/samd/cores/samd21g18a/cortex_handlers.c
@@ -64,6 +64,8 @@ void TCC2_Handler     (void) __attribute__ ((weak, alias("Dummy_Handler")));
 void TC3_Handler      (void) __attribute__ ((weak, alias("Dummy_Handler")));
 void TC4_Handler      (void) __attribute__ ((weak, alias("Dummy_Handler")));
 void TC5_Handler      (void) __attribute__ ((weak, alias("Dummy_Handler")));
+void TC6_Handler      (void) __attribute__ ((weak, alias("Dummy_Handler")));
+void TC7_Handler      (void) __attribute__ ((weak, alias("Dummy_Handler")));
 void ADC_Handler      (void) __attribute__ ((weak, alias("Dummy_Handler")));
 void AC_Handler       (void) __attribute__ ((weak, alias("Dummy_Handler")));
 void DAC_Handler      (void) __attribute__ ((weak, alias("Dummy_Handler")));
@@ -122,6 +124,8 @@ __attribute__ ((section(".isr_vector"))) const DeviceVectors exception_table =
   (void*) TC3_Handler,            /* 18 Basic Timer Counter 0 */
   (void*) TC4_Handler,            /* 19 Basic Timer Counter 1 */
   (void*) TC5_Handler,            /* 20 Basic Timer Counter 2 */
+  (void*) TC6_Handler,            /* 21 Basic Timer Counter 3 */
+  (void*) TC7_Handler,            /* 22 Basic Timer Counter 4 */
   (void*) ADC_Handler,            /* 23 Analog Digital Converter */
   (void*) AC_Handler,             /* 24 Analog Comparators */
   (void*) DAC_Handler,            /* 25 Digital Analog Converter */
